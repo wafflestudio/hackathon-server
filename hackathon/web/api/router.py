4345d83347659b69522d8d6a46c6f1f407f2326b
shinhong-park <shinhong.park@linecorp.com>
2023-02-16 11:44:08 +0900

    Revert "remove dummy apis"
    This reverts commit 2b3cf3cbde2f2746b0bd4feac44d334d215c2587.

--- hackathon/web/api/router.py
+++ hackathon/web/api/router.py
@@ -1,9 +1,12 @@
 from fastapi.routing import APIRouter
 
-from hackathon.web.api import auth, docs, team, user
+from hackathon.web.api import auth, docs, dummy, echo, monitoring, team, user
 
 api_router = APIRouter()
+api_router.include_router(monitoring.router)
 api_router.include_router(docs.router)
+api_router.include_router(echo.router, prefix="/echo", tags=["echo"])
+api_router.include_router(dummy.router, prefix="/dummy", tags=["dummy"])
 api_router.include_router(auth.router, prefix="/auth", tags=["auth"])
 api_router.include_router(team.router, prefix="/teams", tags=["team"])
 api_router.include_router(user.router, prefix="/users", tags=["user"])

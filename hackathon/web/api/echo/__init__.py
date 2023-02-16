2b3cf3cbde2f2746b0bd4feac44d334d215c2587
shinhong-park <shinhong.park@linecorp.com>
2023-02-16 11:30:10 +0900

    remove dummy apis

--- hackathon/web/api/echo/__init__.py
+++
@@ -1,4 +0,0 @@
-"""Echo API."""
-from hackathon.web.api.echo.views import router
-
-__all__ = ["router"]
--- hackathon/web/api/monitoring/__init__.py
+++
@@ -1,4 +0,0 @@
-"""API for checking project status."""
-from hackathon.web.api.monitoring.views import router
-
-__all__ = ["router"]
